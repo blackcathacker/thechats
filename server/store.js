ae7c507b222d5fe6cdfa2f58899b220e599a8f7d
blackcathacker <douglas.pace@gmail.com>
2021-05-02 22:32:30 -0700

    fix issue when log file does not exist

--- server/store.js
+++ server/store.js
@@ -2,20 +2,34 @@ const fs = require('fs')
 const readline = require('readline')
 const { createLogMessage } = require('./util')
 
+// Simple POC "persistent" store leveraging the already existing logging functionality
+// In a real implementation I imagine this would be a real data store of some type (SQL, NoSQL)
+// that would provide actual inserts, indexes, queries, etc.
 function createStore({ logFile } = {}) {
     const store = []
     if (logFile) {
-        const readInterface = readline.createInterface({
-            input: fs.createReadStream(logFile),
-            console: false
-        });
-        readInterface.on('line', l => {
-            try {
-                store.push(JSON.parse(l))
-            } catch (err) {
-                console.error('Unable to process line from log file', err)
-            }
-        })
+        try {
+            fs.access(logFile, fs.constants.R_OK, err => {
+                if (err) {
+                    console.log(`${logFile} does not exist or is not accessible. History will not be available.`)
+                    return
+                }
+                const readInterface = readline.createInterface({
+                    input: fs.createReadStream(logFile),
+                    console: false
+                });
+                readInterface.on('error', () => { })
+                readInterface.on('line', l => {
+                    try {
+                        store.push(JSON.parse(l))
+                    } catch (err) {
+                        console.error('Unable to process line from log file', err)
+                    }
+                })
+            })
+        } catch (err) {
+            console.warn('Unable to read from log file', logFile, err)
+        }
     }
     return {
         put: (client, message) => {
